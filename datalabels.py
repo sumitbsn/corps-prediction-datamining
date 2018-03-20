e712b80fe74c3b06b6ac30842fecda46a118791f
sumitbsn <sumitbsn@gmail.com>
2018-03-20 13:44:29 +0530

    kharif, Rabi and wholeyr season added

--- datalabels.py
+++ datalabels.py
@@ -3,14 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def kharif(rf):
-	khar=rf['June']+rf['July']+rf['August']+rf['September']+rf['October']
+	khar=(rf['June']+rf['July']+rf['August']+rf['September'])/4
 	return khar
 def wholeyear(rf):
-	wy=rf.sum(axis=1)
+	wy=(rf.sum(axis=1))/12
 	return wy
-def tkharif(t):
-	tk=(t['June']+t['July']+t['August']+t['September']+t['October'])/5
-	return tk
+# def tkharif(t):
+# 	tk=(t['June']+t['July']+t['August']+t['September']+t['October'])/5
+# 	return tk
+
+def rabi(val):
+	tr = (val['October']+val['November']+val['December']+val['January']+val['February'])/5
+	return tr
 
 cropdata = pd.read_excel(io='/home/sumit/finalyearproject/data/CropProject.xlsx', sheetname='crop_data')
 
@@ -41,26 +45,78 @@ rfthanjavur=pd.read_excel(io='/home/sumit/finalyearproject/data/rainfall.xlsx',s
 rftiruvallur=pd.read_excel(io='/home/sumit/finalyearproject/data/rainfall.xlsx',sheetname='tiruvallur')
 rftiruvarur=pd.read_excel(io='/home/sumit/finalyearproject/data/rainfall.xlsx',sheetname='tiruvarur')
 
-kharnagapattinam=kharif(rfnagapattinam)
-kharpudukkottai=kharif(rfpudukkottai)
-kharramanathapuram=kharif(rframanathapuram)
-kharsivaganga=kharif(rfsivaganga)
-kharthanjavur=kharif(rfthanjavur)
-khartiruvallur=kharif(rftiruvallur)
-khartiruvarur=kharif(rftiruvarur)
+kharnagapattinam_rainfall=kharif(rfnagapattinam)
+kharpudukkottai_rainfall=kharif(rfpudukkottai)
+kharramanathapuram_rainfall=kharif(rframanathapuram)
+kharsivaganga_rainfall=kharif(rfsivaganga)
+kharthanjavur_rainfall=kharif(rfthanjavur)
+khartiruvallur_rainfall=kharif(rftiruvallur)
+khartiruvarur_rainfall=kharif(rftiruvarur)
 
-wynagapattinam=wholeyear(rfnagapattinam)
-wypudukkottai=wholeyear(rfpudukkottai)
-wyramanathapuram=wholeyear(rframanathapuram)
-wysivaganga=wholeyear(rfsivaganga)
-wythanjavur=wholeyear(rfthanjavur)
-wytiruvallur=wholeyear(rftiruvallur)
-wytiruvarur=wholeyear(rftiruvarur)
+wynagapattinam_rainfall=wholeyear(rfnagapattinam)
+wypudukkottai_rainfall=wholeyear(rfpudukkottai)
+wyramanathapuram_rainfall=wholeyear(rframanathapuram)
+wysivaganga_rainfall=wholeyear(rfsivaganga)
+wythanjavur_rainfall=wholeyear(rfthanjavur)
+wytiruvallur_rainfall=wholeyear(rftiruvallur)
+wytiruvarur_rainfall=wholeyear(rftiruvarur)
 
-tknagapattinam=tkharif(tempnagapattinam)
-tkpudukkottai=tkharif(temppudukkottai)
-tkramanathapuram=tkharif(tempramanathapuram)
-tksivaganga=tkharif(tempsivaganga)
-tkthanjavur=tkharif(tempthanjavur)
-tktiruvallur=tkharif(temptiruvallur)
-tktiruvarur=tkharif(temptiruvarur)
+rbnagapattinam_rainfall=rabi(rfnagapattinam)
+rbpudukkottai_rainfall=rabi(rfpudukkottai)
+rbramanathapuram_rainfall=rabi(rframanathapuram)
+rbsivaganga_rainfall=rabi(rfsivaganga)
+rbthanjavur_rainfall=rabi(rfthanjavur)
+rbtiruvallur_rainfall=rabi(rftiruvallur)
+rbtiruvarur_rainfall=rabi(rftiruvarur)
+
+
+
+kharnagapattinam_temp=kharif(tempnagapattinam)
+kharpudukkottai_temp=kharif(temppudukkottai)
+kharramanathapuram_temp=kharif(tempramanathapuram)
+kharsivaganga_temp=kharif(tempsivaganga)
+kharthanjavur_temp=kharif(tempthanjavur)
+khartiruvallur_temp=kharif(temptiruvallur)
+khartiruvarur_temp=kharif(temptiruvarur)
+
+wynagapattinam_temp=wholeyear(tempnagapattinam)
+wypudukkottai_temp=wholeyear(temppudukkottai)
+wyramanathapuram_temp=wholeyear(tempramanathapuram)
+wysivaganga_temp=wholeyear(tempsivaganga)
+wythanjavur_temp=wholeyear(tempthanjavur)
+wytiruvallur_temp=wholeyear(temptiruvallur)
+wytiruvarur_temp=wholeyear(temptiruvarur)
+
+rbnagapattinam_temp=rabi(tempnagapattinam)
+rbpudukkottai_temp=rabi(temppudukkottai)
+rbramanathapuram_temp=rabi(tempramanathapuram)
+rbsivaganga_temp=rabi(tempsivaganga)
+rbthanjavur_temp=rabi(tempthanjavur)
+rbtiruvallur_temp=rabi(temptiruvallur)
+rbtiruvarur_temp=rabi(temptiruvarur)
+
+print kharpudukkottai_temp, wypudukkottai_temp, rbpudukkottai_temp
+
+kharnagapattinam_press=kharif(pressnagapattinam)
+kharpudukkottai_press=kharif(presspudukkottai)
+kharramanathapuram_press=kharif(pressramanathapuram)
+kharsivaganga_press=kharif(presssivaganga)
+kharthanjavur_press=kharif(pressthanjavur)
+khartiruvallur_press=kharif(presstiruvallur)
+khartiruvarur_press=kharif(presstiruvarur)
+
+wynagapattinam_press=wholeyear(pressnagapattinam)
+wypudukkottai_press=wholeyear(presspudukkottai)
+wyramanathapuram_press=wholeyear(pressramanathapuram)
+wysivaganga_press=wholeyear(presssivaganga)
+wythanjavur_press=wholeyear(pressthanjavur)
+wytiruvallur_press=wholeyear(presstiruvallur)
+wytiruvarur_press=wholeyear(presstiruvarur)
+
+rbnagapattinam_press=rabi(pressnagapattinam)
+rbpudukkottai_press=rabi(presspudukkottai)
+rbramanathapuram_press=rabi(pressramanathapuram)
+rbsivaganga_press=rabi(presssivaganga)
+rbthanjavur_press=rabi(pressthanjavur)
+rbtiruvallur_press=rabi(presstiruvallur)
+rbtiruvarur_press=rabi(presstiruvarur)
